4ce905c43e5e8d61027828314583276f05f0feab
Jinlong Shen <shenjinlong@yahoo.com>
2025-09-22 00:26:45 +0200

    deleted build tree from repo

--- build/CMakeFiles/face_blur_gui_app.dir/src/gui/gui_window.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/face_blur_gui_app.dir/src/gui/gui_window.cpp.o: \
- /home/jinlong/Projects/face-blur-gui-app/src/gui/gui_window.cpp \
- /usr/include/stdc-predef.h \
- /home/jinlong/Projects/face-blur-gui-app/src/gui/gui_window.h
